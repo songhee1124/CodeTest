928c44cd9227498d88826fde419745349b772c64
songhee <zxy4826@naver.com>
2023-12-19 22:52:27 +0900

    codingTest 기초 day5

---
+++ src/Solution1219.java
@@ -0,0 +1,112 @@
+import java.util.Scanner;
+
+public class Solution1219 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    Solution1219 sol = new Solution1219();
+
+    //기초 day5
+        //코드 처리하기
+        String code = "abc1abc1abc";
+        sol.solution(code);
+
+        //등차수열의 특정한 항만 더하기
+//        int a= 3;
+//        int b=4;
+//        boolean[] included ={true, false, false, true, true};
+//        sol.solution1(a,b,included);
+
+        //주사위 게임2
+//        int a =sc.nextInt();
+//        int b =sc.nextInt();
+//        int c= sc.nextInt();
+//
+//        sol.solution2(a,b,c);
+
+        //원소들의 곱과 합
+//        int[] num_list = {3,4,5,2,1};
+//        sol.solution3(num_list);
+
+        //이어 붙인 수
+        int[] num_list ={3,4,5,2,1};
+        sol.solution4(num_list);
+        
+    }
+    //기초 day5
+    //코드 처리하기
+    public String solution(String code) {
+        String answer = "";
+        int mode=0;
+        for (int i = 0; i < code.length(); i++) {
+            if (code.charAt(i) == '1') mode = 1 - mode;
+            else if (i % 2 == mode) answer += code.charAt(i);
+        }
+        System.out.println(answer);
+        return "".equals(answer) ? "EMPTY" : answer;
+    }
+
+    //등차수열의 특정한 항만 더하기
+    public int solution1(int a, int d, boolean[] included) {
+        int answer = 0;
+
+        for(int i=0; i<included.length; i++){
+            if(included[i]){
+                answer += a+(d*i);
+            }
+        }
+        System.out.println(answer);
+        return answer;
+    }
+
+    //주사위 게임
+    public int solution2(int a, int b, int c) {
+        int answer = 0;
+
+        if(a==b&&b==c){
+            answer = (a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        }else if(a==b&&b!=c||a==c&&c!=b||a!=b&&b==c){
+            answer = (a+b+c)*(a*a+b*b+c*c);
+        }else{
+            answer = a+b+c;
+        }
+        System.out.println(answer);
+        return answer;
+    }
+
+    //원소들의 곱과 합
+    public int solution3(int[] num_list) {
+        int answer = 0;
+        int sum = 0;
+        int mul = 1;
+        for(int i = 0; i<num_list.length;i++){
+            sum+=num_list[i];
+            mul*=num_list[i];
+        }
+        if(sum * sum > mul){
+            answer = 1;
+        }
+        else
+            answer=0;
+        System.out.println(answer);
+        return answer;
+    }
+
+    //이어붙인 수
+    public int solution4(int[] num_list) {
+        int answer = 0;
+        String odd="";
+        String even="";
+        for(int i=0; i<num_list.length; i++){
+            if(num_list[i]%2==0){
+                 even +=Integer.toString(num_list[i]);
+            }
+            else {
+                odd += Integer.toString(num_list[i]);
+            }
+
+        }
+        answer = Integer.parseInt(even) + Integer.parseInt(odd);
+        System.out.println(answer);
+        return answer;
+    }
+}
